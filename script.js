29ebd6b92a6a6cc0149409f357ff619657577111
Ivanishkina-li <105155805+Ivanishkina-li@users.noreply.github.com>
2022-09-18 16:18:21 +0300

    task2

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let myArray = ["a", "b", "c", "d"];
 
 // Only change code above this line
 console.log(myArray);
+
+function mixedNumbers(arr) {
+  // Only change code below this line
+  arr.push(7, "VIII", 9);
+  arr.unshift("I", 2, "three");
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(["IV", 5, "six"]));
