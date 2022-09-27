4144d902eac120f9d87e704ef565b89ce709cb4d
Ivanishkina-li <105155805+Ivanishkina-li@users.noreply.github.com>
2022-09-27 10:26:08 +0300

    lesson2

--- node.js/node.js
+++ node.js/node.js
@@ -1,8 +1,25 @@
-const http = require("http"); //подключи в наш файл  объект http
+// const http = require("http"); //подключи в наш файл  объект http
 
-const server = http.createServer((request, response) => {
-  response.write("it ");
-  response.end();
-});
-server.listen(3003); // даем установку серверу слушать конкр порт
+// const server = http.createServer((request, response) => {
+//   response.write("it ");
+//   response.end();
+// });
+//server.listen(3003); // даем установку серверу слушать конкр порт
 //http://localhost:3003/   пишем в поисковикепше
+
+const http = require("http");
+const server = http.createServer((request, response) => {
+  switch (request.url) {
+    case "/home": {
+      response.write("best free online courses");
+      response.end();
+      break;
+    }
+    default: {
+      response.write("404");
+      response.end();
+    }
+  }
+});
+
+server.listen(3003);
