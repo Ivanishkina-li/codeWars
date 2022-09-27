8e011fcc14d07fecb7660bb92828078cc747f8e6
Ivanishkina-li <105155805+Ivanishkina-li@users.noreply.github.com>
2022-09-27 20:13:51 +0300

    lesson

--- node.js/node.js
+++ node.js/node.js
@@ -7,12 +7,20 @@
 //server.listen(3003); // даем установку серверу слушать конкр порт
 //http://localhost:3003/   пишем в поисковикепше
 
+const fs = require("fs");
 const http = require("http");
 const server = http.createServer((request, response) => {
   switch (request.url) {
     case "/home": {
       response.write("best free online courses");
       response.end();
+      break;
+    }
+    case "/about": {
+      setTimeout(() => {
+        response.write("about");
+        response.end();
+      }, 3000);
       break;
     }
     default: {
