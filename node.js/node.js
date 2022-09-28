76a9a450159296d0348eea98f22a4fe77a6734db
Ivanishkina-li <105155805+Ivanishkina-li@users.noreply.github.com>
2022-09-28 15:23:38 +0300

    updete

--- node.js/node.js
+++ node.js/node.js
@@ -7,12 +7,20 @@
 //server.listen(3003); // даем установку серверу слушать конкр порт
 //http://localhost:3003/   пишем в поисковикепше
 
-const fs = require("fs");
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((request, response) => {
   switch (request.url) {
     case "/home": {
-      response.write("best free online courses");
+      //когла я прочитаб эти данные я вызову колл бэк
+      fs.readFile("./index.html", (err, data) => {
+        if (err) {
+          response.write(err);
+        } else {
+          response.write(data);
+        }
+      });
+
       response.end();
       break;
     }
@@ -31,3 +39,24 @@ const server = http.createServer((request, response) => {
 });
 
 server.listen(3003);
+
+//скопировали с доки экспресса
+const express = require("express"); // требуем экспресс
+const app = express(); //создади прил
+const port = 3000; //указали порт
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  res.send("hello samutai");
+});
+
+app.post("/users", (req, res) => {
+  res.send("we are created samurais");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
