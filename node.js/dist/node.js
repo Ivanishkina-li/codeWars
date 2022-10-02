35c566d2ec97bdb85acd21dc985faba197907c07
Ivanishkina-li <105155805+Ivanishkina-li@users.noreply.github.com>
2022-10-02 18:58:13 +0300

    update package.json

---
+++ node.js/dist/node.js
@@ -0,0 +1,126 @@
+"use strict";
+// const http = require("http"); //подключи в наш файл  объект http
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const server = http.createServer((request, response) => {
+//   response.write("it ");
+//   response.end();
+// });
+//server.listen(3003); // даем установку серверу слушать конкр порт
+//http://localhost:3003/   пишем в поисковикепше
+// const delay = (ms) => {
+//   const promise = new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res();
+//     }, ms);
+//   });
+// };
+// const readFile = (path) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(path, (err, res) => {
+//       if (err) {
+//         reject(err);
+//       } else resolve(res);
+//     });
+//   });
+// };
+// const http = require("http");
+// const fs = require("fs");
+// const server = http.createServer(async (request, response) => {
+//   switch (request.url) {
+//     case "/home": {
+//       //когла я прочитаб эти данные я вызову колл бэк то есть асинхронно
+//       try {
+//         const data = await readFile("./index.html");
+//         response.write(data);
+//         response.end();
+//       } catch (err) {
+//         response.write("error");
+//         response.end();
+//       }
+//       break;
+//     }
+//     case "/about": {
+//       await delay(3000);
+//       response.write("about");
+//       response.end();
+//       break;
+//     }
+//     default: {
+//       response.write("404");
+//       response.end();
+//     }
+//   }
+// });
+// server.listen(3003);
+//скопировали с доки экспресса
+//const express = require("express"); // требуем экспресс
+// import express from "express";
+// const app = express(); //создади прил
+// const port = 3000; //указали порт
+// app.get("/", (req, res) => {
+//   let a = 4;
+//   if (a > 5) {
+//     res.send("ok");
+//   } else {
+//     res.send("Hello World!!");
+//   }
+// });
+// app.get("/users", (req, res) => {
+//   res.send("hello samutai");
+// });
+// app.post("/users", (req, res) => {
+//   res.send("we are created samurais");
+// });
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+// app.get("/", (req, res) => {
+//   res.send({ message: "it-incubatorr" });
+// });
+//вводим это в консоли по ссылке на порт fetch('http://localhost:3000', {method:'GET'}).then(res => res.json()).then(data => console.log(data))
+// и получим ответ {message: 'it-incubator'}
+app.get("/courses", (req, res) => {
+    res.json([
+        { id: 1, title: "frontend" },
+        { id: 2, title: "backend" },
+        { id: 3, title: "automation qa" },
+        { id: 4, title: "devops" },
+    ]);
+});
+//ответ
+// 4) [{…}, {…}, {…}, {…}]
+// 0
+// :
+// {id: 1, title: 'frontend'}
+// 1
+// :
+// {id: 2, title: 'backend'}
+// 2
+// :
+// {id: 3, title: 'automation qa'}
+// 3
+// :
+// {id: 4, title: 'devops'}
+app.get("/courses/:id", (req, res) => {
+    const FoundCourse = [
+        { id: 1, title: "frontend" },
+        { id: 2, title: "backend" },
+        { id: 3, title: "automation qa" },
+        { id: 4, title: "devops" },
+    ].find((c) => c.id === +req.params.id);
+    if (!FoundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(FoundCourse);
+});
+//http://localhost:3000/courses/3   получим  { id: 3, title: "automation qa" }
+app.listen(port, () => {
+    console.log(`exaple port listening on ${port}`);
+});
--- node.js/node.js
+++ node.js/node.js
@@ -1,62 +1,77 @@
+"use strict";
 // const http = require("http"); //подключи в наш файл  объект http
-
+exports.__esModule = true;
 // const server = http.createServer((request, response) => {
 //   response.write("it ");
 //   response.end();
 // });
 //server.listen(3003); // даем установку серверу слушать конкр порт
 //http://localhost:3003/   пишем в поисковикепше
-
-const http = require("http");
-const fs = require("fs");
-const server = http.createServer((request, response) => {
-  switch (request.url) {
-    case "/home": {
-      //когла я прочитаб эти данные я вызову колл бэк
-      fs.readFile("./index.html", (err, data) => {
-        if (err) {
-          response.write(err);
-        } else {
-          response.write(data);
-        }
-      });
-
-      response.end();
-      break;
-    }
-    case "/about": {
-      setTimeout(() => {
-        response.write("about");
-        response.end();
-      }, 3000);
-      break;
-    }
-    default: {
-      response.write("404");
-      response.end();
-    }
-  }
-});
-
-server.listen(3003);
-
+// const delay = (ms) => {
+//   const promise = new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res();
+//     }, ms);
+//   });
+// };
+// const readFile = (path) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(path, (err, res) => {
+//       if (err) {
+//         reject(err);
+//       } else resolve(res);
+//     });
+//   });
+// };
+// const http = require("http");
+// const fs = require("fs");
+// const server = http.createServer(async (request, response) => {
+//   switch (request.url) {
+//     case "/home": {
+//       //когла я прочитаб эти данные я вызову колл бэк то есть асинхронно
+//       try {
+//         const data = await readFile("./index.html");
+//         response.write(data);
+//         response.end();
+//       } catch (err) {
+//         response.write("error");
+//         response.end();
+//       }
+//       break;
+//     }
+//     case "/about": {
+//       await delay(3000);
+//       response.write("about");
+//       response.end();
+//       break;
+//     }
+//     default: {
+//       response.write("404");
+//       response.end();
+//     }
+//   }
+// });
+// server.listen(3003);
 //скопировали с доки экспресса
-const express = require("express"); // требуем экспресс
-const app = express(); //создади прил
-const port = 3000; //указали порт
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//const express = require("express"); // требуем экспресс
+var express_1 = require("express");
+var app = (0, express_1["default"])(); //создади прил
+var port = 3000; //указали порт
+app.get("/", function (req, res) {
+    var a = 8;
+    if (a > 5) {
+        res.send("ok");
+    }
+    else {
+        res.send("Hello World!");
+    }
 });
-
-app.get("/users", (req, res) => {
-  res.send("hello samutai");
+app.get("/users", function (req, res) {
+    res.send("hello samutai");
 });
-
-app.post("/users", (req, res) => {
-  res.send("we are created samurais");
+app.post("/users", function (req, res) {
+    res.send("we are created samurais");
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, function () {
+    console.log("Example app listening on port ".concat(port));
 });
